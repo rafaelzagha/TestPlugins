825f9a026ee3e372394ac4016df9252a618c4570
rafaezagha <rafael.albagli.zagha@gmail.com>
2025-03-20 10:01:32 +0200

    Fixing the fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,9 +36,8 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_WORKSPACE") ?: "https://github.com/rafaelzagha/TestPlugins")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/rafaelzagha/TestPlugins")
     }
-
 
     android {
         defaultConfig {
